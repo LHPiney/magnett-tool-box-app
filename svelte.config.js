f403f0580710a4961a6ad4d6030a13c94626f15e
Luis Hernández Piney <luis.piney@gmail.com>
2023-11-17 21:11:32 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
