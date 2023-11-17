5671c23b9f8faf48d0a9287b2a13dd631797b756
Luis Hernández Piney <luis.piney@gmail.com>
2023-11-17 22:50:36 +0100

    Add azure adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+
+import azure from 'svelte-adapter-azure-swa';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'null',
+		adapter: azure({	
+			comStaticWebAppConfig: {
+				routes: [
+					{ 
+						route: '/api/*', 
+						methods: ['GET', 'POST', 'PUT', 'DELETE'],
+						allowedRoles: ['anonymous'] 
+					},
+				], 
+				mimeTypes :  {'.json': 'application/json'}
+			}
 		})
 	}
 };
