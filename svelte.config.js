28470e2b5bd40be073364abb4b427c9ffdfeda43
Luis Hernández Piney <luis.piney@gmail.com>
2023-11-17 23:13:56 +0100

    Change adapter config agin

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,9 @@
-
 import azure from 'svelte-adapter-azure-swa';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
 	kit: {
-		adapter: azure({	
-			comStaticWebAppConfig: {
-				routes: [
-					{ 
-						route: '/api/*', 
-						methods: ['GET', 'POST', 'PUT', 'DELETE'],
-						allowedRoles: ['anonymous'] 
-					},
-				], 
-				mimeTypes :  {'.json': 'application/json'}
-			}
-		})
+		adapter: azure()
 	}
 };
 
