62b5108822500a4311dadeab89b50eadf4dfbeb1
Luis Hernández Piney <luis.piney@gmail.com>
2023-11-18 13:58:38 +0100

    Generate app

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import azure from 'svelte-adapter-azure-swa';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte'],
+	preprocess: [ vitePreprocess()],
+	vitePlugin: {
+		inspector: true,
+	},
 	kit: {
 		adapter: azure()
 	}
 };
-
 export default config;
