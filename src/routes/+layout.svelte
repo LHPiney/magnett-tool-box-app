51cf8c2bbd7fe60ea447a88b6e6ae9154f2a871a
Luis Hernández Piney <luis.piney@gmail.com>
2023-11-19 15:00:05 +0100

    Reomove last template stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,36 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let valueSingle: string = 'books';
 </script>
 
 <!-- App Shell -->
-<AppShell>
+<AppShell regionPage="relative" slotPageHeader="sticky top-10 z-10">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Developer Tools Box</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
+					class="btn btn-sm variant-ghost-surface rounded-full"
+					href="https://twitter.com/LHPiney"
 					target="_blank"
 					rel="noreferrer"
 				>
 					Twitter
 				</a>
 				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					class="btn btn-sm variant-ghost-surface rounded-full"
+					href="https://github.com/LHPiney/magnett-tool-box-app"
 					target="_blank"
 					rel="noreferrer"
 				>
@@ -44,6 +39,10 @@
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<!-- Router Slot -->
 	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
 </AppShell>
